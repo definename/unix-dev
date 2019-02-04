97a3515bd3aa7bfad0d3ab443466a0eaebeb802c
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2019-02-04 18:43:10 +0200

    Added simple make sample.

---
+++ make-sample/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "util.h"
+
+int main(int argc, char* argv[])
+{
+	std::cout << "Hello MAKE!! " << Func() << std::endl;
+	return 0;
+}
---
+++ make-sample/util.cpp
@@ -0,0 +1,4 @@
+int Func()
+{
+	return 100;
+}
